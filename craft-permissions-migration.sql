b01cee90d187d57d65455716d2abdf1a7c3a1946
Prophecyte <kittenchow09@gmail.com>
2026-02-16 00:59:02 -0500

    Delete craft-permissions-migration.sql

--- craft-permissions-migration.sql
+++
@@ -1,8 +0,0 @@
--- Craft Room Permissions Migration
--- Run on your existing Neon database
-
-ALTER TABLE craft_room_members ADD COLUMN IF NOT EXISTS role VARCHAR(20) DEFAULT 'viewer';
-ALTER TABLE craft_room_members ADD COLUMN IF NOT EXISTS can_view_hidden BOOLEAN DEFAULT false;
-
--- Set existing owners to 'owner' role
-UPDATE craft_room_members SET role = 'owner' WHERE is_owner = true;
